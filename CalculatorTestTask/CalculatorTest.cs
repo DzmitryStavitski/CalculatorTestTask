d170f4ae8b1a3cbc6200c5e9fb81ccad412605e7
Fral102 <FralStavitskiy@gmail.com>
2021-03-26 08:27:10 +0300

    Add project files.

---
+++ CalculatorTestTask/CalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorTestTask
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
