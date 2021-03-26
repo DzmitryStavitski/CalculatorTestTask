df2c1b53c402eda8aca2c1574bc5e78c8b8ae7da
Stavitsky <d.stavitsky@a1qa.com>
2021-03-26 11:27:44 +0300

    Add base structure

---
+++ CalculatorTestTask/Tests/CalculatorTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Automation;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+
+namespace CalculatorTest
+{
+    [TestClass]
+    class UnitTest1
+    {
+        private static Application application;
+
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            application = Application.Launch(@"C:\Windows\System32\calc112.exe");
+            Assert.IsNotNull(application);
+
+            GetButton("1").Click();
+            GetButton("2").Click();
+            GetButton("Add").Click();
+            GetButton("9").Click();
+            GetButton("9").Click();
+            GetButton("9").Click();
+            GetButton("Equals").Click();
+            GetButton("Memory add").Click();
+            GetButton("1").Click();
+            GetButton("9").Click();
+            GetButton("Add").Click();
+            GetButton("Memory recall").Click();
+            GetButton("Equals").Click();
+
+            Assert.AreEqual("1030", ResultTextBox());
+            application.Close();
+        }
+
+        private static Button GetButton(string elementText)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var button = GetWindow().Get<Button>(SearchCriteria.ByText(elementText));
+                if (button != null) return button;
+            }
+
+            return null;
+        }
+
+        private static Window GetWindow()
+        {
+            return application.GetWindow("Calculator");
+            
+        }
+
+        private static string ResultTextBox()
+        {
+            return
+                GetWindow().Get<Label>(SearchCriteria.ByAutomationId("150")).AutomationElement.
+                    GetCurrentPropertyValue(AutomationElement.NameProperty).ToString();
+        }
+    }
+}
---
+++ Framework/Application/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+
+namespace Framework.Application
+{
+    class Application
+    {
+        private TestStack.White.Application application;
+        private string applicationPath;
+
+        public Application(string applicationPath)
+        {
+            this.applicationPath = applicationPath;
+        }
+
+        public bool Run()
+        {
+            application = TestStack.White.Application.Launch(applicationPath);
+            return application == null;
+        }
+    }
+}
