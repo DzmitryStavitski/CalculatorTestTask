ebc19b0e0fbaf3c0f0468c24eeb1a780cd0ce728
Fral102 <FralStavitskiy@gmail.com>
2021-03-27 15:05:39 +0300

    Add working SpecFlow tests

---
+++ CalculatorSpecFlowTests/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,45 @@
+﻿using CalculatorSpecFlowTests.Drivers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CalculatorSpecFlowTests.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        CalculatorObject calculater = null;
+
+        [Given("open the calculator")]
+        public void OpenTheCalculator()
+        {
+            calculater = new CalculatorObject();
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            calculater.EnterNumber(number);
+            calculater.ClickAddButton();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            calculater.EnterNumber(number);
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            calculater.ClickEqualsButton();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Assert.AreEqual(result.ToString(), calculater.getResult());
+            calculater.Close();
+        }
+    }
+}
